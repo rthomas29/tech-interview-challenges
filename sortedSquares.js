9febda7ab74ad2a80c8f695fb59357639f11e70b
Rakeem <thomas.rakeem.29@gmail.com>
2019-05-05 22:01:59 -0400

    typo

--- sortedSquares.js
+++ sortedSquares.js
@@ -13,4 +13,4 @@
 const sortedSquares = A => A.map(num => num * num).sort((a, b) => a - b)
 
 sortedSquares([-4, -1, 0, 3, 10]) // => [0, 1, 9, 16, 100]
-sortedSquares([5, 4, 3, 2, 1]) // => [1, 4, 9, 16, 25]
+console.log(sortedSquares([1, 2, 3, 4, 5])) // => [1, 4, 9, 16, 25]
